45b9c38c55460677c9cd5308d27a82aae804d8bb
Jonatas Oliveira <oliveira.jonatas@outlook.com.br>
2021-02-24 23:14:18 -0300

    Reformulação
    reformulação da lógica e adicionado botão de exclusão de times,

--- script.js
+++ script.js
@@ -2,54 +2,49 @@ let timeVetor = []
    
 
 function limpar(){
-    document.getElementById('txtCadastro').value = ''
-    document.getElementById('times').value = ''
+    document.getElementById('txtCadastro').value = '' //realiza a limpeza da caixa texto
 }
 
 
 function cadastrar(){
-    let cadastro = document.getElementById('txtCadastro').value
-    let opcao = document.createElement('option')
-    let times = document.getElementById('times')
-    opcao.setAttribute('value',cadastro)
-    opcao.innerText = cadastro
-    times.appendChild(opcao)
-
-    limpar() 
+    let sTimes = document.getElementById('sTimes')
+    let time = document.getElementById('txtCadastro').value
+    timeVetor.push(time) //adiciona time no array
+    let opcao = document.createElement('option') //cria novo option no select
+    opcao.setAttribute('value',time) //configura os atributos do option
+    opcao.innerText = time 
+    sTimes.appendChild(opcao) //linka o option com o select
+    
+    sTimes.innerHTML += `${timeVetor[timeVetor.length-1]}\n`   
 }
 
 
 
+function excluir(){
+    let sTimes = document.getElementById('sTimes')
+    let index = document.getElementById("sTimes").selectedIndex //atribui a variavel o indice selecionado no select
+    timeVetor.splice(index,1) //remove a variavel do array através do indice informado
+    sTimes.remove(sTimes.selectedIndex) //remove o option do select
 
-
-
-function inserir(){
-    let times = document.getElementById('times').value
-    let tArea = document.getElementById('tArea')
-
-    if((timeVetor.indexOf(times))==-1){
-        timeVetor.push(times)
-        tArea.innerHTML += `${times}\n`
-        limpar()
-    }else{
-        window.alert('Time já incluido!')
-    }
+      
+   
 }
 
 
 function gerar(){
-    tArea.innerHTML = ''
-
     let saida = document.getElementById('saida')
-    
+    sTimes.innerHTML = ''
+    saida.innerHTML = ''
+   
     saida.innerHTML += `<p>CONFRONTOS</p>`
 
     for(let c=0;c<=timeVetor.length-1;c++){
         for(let i=0;i<=timeVetor.length-1;i++){ 
             if(timeVetor[c] != timeVetor[i]){
-                saida.innerHTML += `${timeVetor[c]} x ${timeVetor[i]}<br>`
+                saida.innerHTML += `${timeVetor[c]} x ${timeVetor[i]}`
             }   
         }
         saida.innerHTML += `<br>`
     }
+    timeVetor = []
 }
