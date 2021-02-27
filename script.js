d877cc0e16842361b898049cece7b7b59faeb20a
Jonatas Oliveira <oliveira.jonatas@outlook.com.br>
2021-02-26 22:43:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,13 +9,19 @@ function limpar(){
 function cadastrar(){
     let sTimes = document.getElementById('sTimes')
     let time = document.getElementById('txtCadastro').value
-    timeVetor.push(time) //adiciona time no array
-    let opcao = document.createElement('option') //cria novo option no select
-    opcao.setAttribute('value',time) //configura os atributos do option
-    opcao.innerText = time 
-    sTimes.appendChild(opcao) //linka o option com o select
-    
-    sTimes.innerHTML += `${timeVetor[timeVetor.length-1]}\n`   
+
+    //Verificar se caixa de texto está preenchida
+    if(time == ''){
+        alert('[ATENÇÃO] - Favor informar um time.')
+    }else{
+        timeVetor.push(time) //adiciona time no array
+        let opcao = document.createElement('option') //cria novo option no select
+        opcao.setAttribute('value',time) //configura os atributos do option
+        opcao.innerText = time 
+        sTimes.appendChild(opcao) //linka o option com o select
+        
+        sTimes.innerHTML += `${timeVetor[timeVetor.length-1]}\n` 
+    }     
 }
 
 
@@ -23,28 +29,36 @@ function cadastrar(){
 function excluir(){
     let sTimes = document.getElementById('sTimes')
     let index = document.getElementById("sTimes").selectedIndex //atribui a variavel o indice selecionado no select
-    timeVetor.splice(index,1) //remove a variavel do array através do indice informado
-    sTimes.remove(sTimes.selectedIndex) //remove o option do select
 
-      
-   
+    //verifica se existe time selecionado para exclusão
+    if(index == -1){
+        alert('[ATENÇÃO] Favor selecionar time para exclusão.')
+    }else{
+        timeVetor.splice(index,1) //remove a variavel do array através do indice informado
+        sTimes.remove(sTimes.selectedIndex) //remove o option do select
+    }
 }
 
 
 function gerar(){
     let saida = document.getElementById('saida')
-    sTimes.innerHTML = ''
-    saida.innerHTML = ''
+    //saida.innerHTML = ''
    
-    saida.innerHTML += `<p>CONFRONTOS</p>`
-
-    for(let c=0;c<=timeVetor.length-1;c++){
-        for(let i=0;i<=timeVetor.length-1;i++){ 
-            if(timeVetor[c] != timeVetor[i]){
-                saida.innerHTML += `${timeVetor[c]} x ${timeVetor[i]}`
-            }   
+    if(timeVetor.length<2){ 
+        alert('[ATENÇÃO] Quantidade insuficiente de times.')
+    }else{
+        saida.innerHTML += `<p>CONFRONTOS</p>`
+        for(let c=0;c<=timeVetor.length-1;c++){
+            for(let i=0;i<=timeVetor.length-1;i++){ 
+                if(timeVetor[c] != timeVetor[i]){
+                    saida.innerHTML += `${timeVetor[c]} x ${timeVetor[i]}<br>`
+                } 
+               saida.innerHTML +=  
+            }
+          
         }
-        saida.innerHTML += `<br>`
+        timeVetor=[]
+        sTimes.innerHTML = ''
     }
-    timeVetor = []
+
 }
